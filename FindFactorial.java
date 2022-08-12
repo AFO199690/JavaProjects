c4f7c1d632c29a168f7606042b4f606a37e2299b
AFO199690 <110529890+AFO199690@users.noreply.github.com>
2022-08-12 16:55:28 +0530

    Add files via upload

---
+++ FindFactorial.java
@@ -0,0 +1,16 @@
+Author@ chesta(w.a.p to remove whitespace from a string)
+import java.util.*;
+public class Main
+{
+     public static void main(String []args)
+     {
+         //Take input from the user
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the String :");
+        String str=sc.nextLine();    //Initialize the String
+        
+        str = str.replaceAll("\\s", ""); 
+       
+        System.out.println("After Removing Whitespaces the entered string is:"+str); 
+     }
+}
