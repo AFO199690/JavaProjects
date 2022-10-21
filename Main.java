d0d1704cc71b784d0ca0703543f3cb0ca31de6b2
AFO199690 <110529890+AFO199690@users.noreply.github.com>
2022-10-21 07:39:35 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,18 +1,144 @@
-package inheritance;
-//main class 
-	public class Main 
+
+package com.User_Mgt_Project;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+
+
+public class users {
+	
+	//Insert users
+
+	public static boolean insertusers(String uname)
 	{
-//main method called
-		public static void main(String args[])
-		{
-			Square s1=new Square();
-			s1.area();
-			System.out.println("The area of the square is : "+s1.area());//print(length*length)
+		try {
 			
-			Rectangle s2=new Rectangle();
-			s2.area();
-			System.out.println("The area of the rectangle is : "+s2.area());//print(length*width)
+			Connection con=ConnectionProvider.createConnection();
+			PreparedStatement pstmt=con.prepareStatement("insert into users (uname)values(?)");
+			pstmt.setString(1, uname);
+			//pstmt.setString(1, password);
+		    
+		     
+		     pstmt.executeUpdate();
+		     
+		     return true;
+		  
+		   
+		    
+			}catch(Exception e) {
+				e.printStackTrace();
+				
+			}
+		return false;
 		}
-	}
-Footer
-    
+		
+		
+	
+	//update password
+		public static boolean updateusers(String password1) {
+			try {
+				
+			Connection con=ConnectionProvider.createConnection();
+			PreparedStatement pstmt=con.prepareStatement("update users set password=XYZABZ (password)values(?)");
+			pstmt.setString(1, password1);
+
+		    
+		     
+		     pstmt.executeUpdate();
+		     return true;
+		  
+		   
+		    
+			}catch(Exception e) {
+				e.printStackTrace();
+				
+			}
+			
+			
+			
+			return false;
+
+		}
+		
+		//delete users
+		
+			public static boolean deleteusers(String uname) {
+				try {
+					
+				Connection con=ConnectionProvider.createConnection();
+				PreparedStatement pstmt=con.prepareStatement("alter table users drop uname (uname)values(?)");
+				pstmt.setString(1, uname);
+
+			    
+			     
+			     pstmt.executeUpdate();
+			     return true;
+			  
+			   
+			    
+				}catch(Exception e) {
+					e.printStackTrace();
+					}
+				
+				return false;
+
+			}
+			
+			//Retrieve a User
+			public static boolean retrieveusers(String uname) {
+				try {
+					
+				Connection con=ConnectionProvider.createConnection();
+				PreparedStatement pstmt=con.prepareStatement("select uname from users (uname)values(?)");
+				pstmt.setString(1, uname);
+
+			    
+			     
+			     pstmt.executeUpdate();
+			     return true;
+			  
+			   
+			    
+				}catch(Exception e) {
+					e.printStackTrace();
+					}
+				
+				return false;
+
+			}
+			
+			//See users list
+			public static boolean Showusers(String uname) {
+				try {
+					
+				Connection con=ConnectionProvider.createConnection();
+				PreparedStatement pstmt=con.prepareStatement("select *from users (uname)values(?)");
+				pstmt.setString(1, uname);
+
+			    
+			     
+			     pstmt.executeUpdate();
+			     return true;
+			  
+			   
+			    
+				}catch(Exception e) {
+					e.printStackTrace();
+					}
+				
+				return false;
+
+			}
+			
+			//Exit
+			
+
+
+			
+
+		}
+
+	
+
+   
